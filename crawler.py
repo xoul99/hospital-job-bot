1570e3d423b12a94c4b99818ac1de11d691e464a
xoul99 <xoul95@gmail.com>
2026-06-11 09:25:59 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -19,12 +19,12 @@ KEYWORDS = [
 ]
 
 def send_telegram(message):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot8423721416:AAFwBou72UpQeT9axHy7QXem2CGfbBfTQlo/sendMessage"
 
     requests.post(
         url,
         data={
-            "chat_id": CHAT_ID,
+            "chat_id": 8506993219,
             "text": message
         }
     )
